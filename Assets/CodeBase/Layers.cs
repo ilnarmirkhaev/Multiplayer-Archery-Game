a0d50b7bf07669f969074d96de0aa88a0221d2bf
ilnarmirkhaev <ilnar0806@mail.ru>
2022-12-18 00:14:41 +0300

    fix: add Players layer

--- Assets/CodeBase/Layers.cs
+++ Assets/CodeBase/Layers.cs
@@ -7,6 +7,7 @@ namespace CodeBase
         private static readonly int ArrowsLayerMask = LayerMask.GetMask("Arrows");
         private static readonly int WorldBoundsLayerMask = LayerMask.GetMask("WorldBoundaries");
 
-        public static readonly int ArrowsMask = ~(ArrowsLayerMask | WorldBoundsLayerMask);
+        public static readonly int ArrowsMask = ~LayerMask.GetMask("Arrows", "WorldBoundaries");
+        public static readonly int PlayersLayer = LayerMask.NameToLayer("Players");
     }
 }
