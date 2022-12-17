b5fc26ea3fe0623fc95046afe549b7e56993c45e
ilnarmirkhaev <ilnar0806@mail.ru>
2022-12-18 00:18:48 +0300

    feat: add NetworkSingleton generic class

---
+++ Assets/CodeBase/Network/NetworkSingleton.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace CodeBase.Network
+{
+    public class NetworkSingleton<T> : NetworkBehaviour where T : Component
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance is null)
+                {
+                    T[] objects = FindObjectsOfType<T>();
+                    
+                    if (objects.Length > 0)
+                        _instance = objects[0];
+                    
+                    if (objects.Length > 1)
+                        Debug.LogError("There is more than one " + typeof(T).Name + " in the scene!");
+
+                    if (_instance is null)
+                    {
+                        GameObject obj = new GameObject
+                        {
+                            name = $"_{typeof(T).Name}"
+                        };
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
