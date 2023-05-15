e7f688a3e98294e9818d81ad16fa56fccccae3ca
ilnarmirkhaev <ilnar0806@mail.ru>
2023-05-15 14:11:27 +0300

    Add clamping to health value

--- Assets/Scripts/Photon/HealthDisplay.cs
+++ Assets/Scripts/Photon/HealthDisplay.cs
@@ -8,14 +8,19 @@ namespace Photon
         [SerializeField] private Slider slider;
         [SerializeField] private Text text;
 
+        private int _max;
+
         public void Init(int maxHealth)
         {
+            _max = maxHealth;
+            slider.minValue = 0;
             slider.maxValue = maxHealth;
             UpdateHealth(maxHealth);
         }
 
         public void UpdateHealth(int currentHealth)
         {
+            currentHealth = Mathf.Clamp(currentHealth, 0, _max);
             slider.value = currentHealth;
             text.text = currentHealth.ToString();
         }
